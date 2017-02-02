db0ee6a37a12f25e81766de1e6751f09bf6d02a9
Valentin OMNES <vomnes@student.42.fr>
2017-02-02 18:04:38 +0100

    Add start

--- filler.h
+++ filler.h
@@ -14,8 +14,20 @@
 # define FILLER_H
 
 # include "libft.h"
+# include "ft_printf.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
+
+# define BUFF_SIZE 48
+# define NB_MAX_FD 4864
+
+typedef struct  s_data
+{
+    int         xy_plateau[2];
+    int         xy_piece[2];
+}               t_data;
+
+int				get_next_line(const int fd, char **line);
 
 #endif
--- main.c
+++ main.c
@@ -11,3 +11,62 @@
 /* ************************************************************************** */
 
 # include "filler.h"
+
+void ft_get_xy(char *input, int *stock)
+{
+    int i;
+    int next;
+
+    i = 0;
+    next = 0;
+    stock[0] = 0;
+    stock[1] = 0;
+    while (input[i] != '\0')
+    {
+        while (ft_isdigit(input[i]))
+            stock[next] = stock[next] * 10 + (input[i++] - '0');
+        if (ft_isdigit(input[i - 1]) && input[i] != ':')
+            next++;
+        i++;
+    }
+}
+
+int main()
+{
+    char *line;
+    t_data data;
+
+    get_next_line(0, &line);
+    if ((ft_strstr(line, "Plateau")))
+        ft_get_xy(line, data.xy_plateau);
+    else if ((ft_strstr(line, "Piece")))
+        ft_get_xy(line, data.xy_piece);
+    ft_printf("data.xy_plateau[0] : %d - data.xy_plateau[1] : %d\n", data.xy_plateau[0], data.xy_plateau[1]);
+    ft_printf("data.xy_piece[0] : %d - data.xy_piece[1] : %d\n", data.xy_piece[0], data.xy_piece[1]);
+    return (0);
+}
+
+
+/*
+Plateau 15 17:
+    01234567890123456
+000 XXXXXXXXXXXXXXXX.
+001 XXXXXXXXXXXXXXXX.
+002 XXXXXXXXXXXXXXXX.
+003 XXXXXXXXXXXXXXXX.
+004 XXXXXXXXXXXXXXOOO
+005 ..OX.XXOOOOOOOOOO
+006 oooXXXXXOOOOOOOOO
+007 .OOOOOXXOOOOOOOOO
+008 .OOOOXXXXXOOOOOOO
+009 OOOOOOOOXXOOOOOOO
+010 .OOOOXXXXXOOOOOOO
+011 .OOOOOOOXXOOOOOOO
+012 OOOOOOOXXXXXXXXOO
+013 OOOOOOOOOOOOOOOOO
+014 ..OOOOOOOOOOOOOOO
+Piece 2 3:
+..*
+.**
+<got (O): [0, 0]
+*/
