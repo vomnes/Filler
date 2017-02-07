9b597984eb15335f114ecc323dd131fa93520107
Valentin OMNES <vomnes@student.42.fr>
2017-02-07 19:20:36 +0100

    ok stock[next]

--- filler.c
+++ filler.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vomnes <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/07 17:48:20 by vomnes            #+#    #+#             */
+/*   Updated: 2017/02/07 19:15:29 by vomnes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 # include "filler.h"
 
 void ft_print_strtab(char **tab)
@@ -31,14 +43,32 @@ static void ft_get_xy(char *input, int *stock)
     next = 0;
     stock[0] = 0;
     stock[1] = 0;
+	dprintf(2, "input >> %s | next : %d\n", input, next);
     while (input[i] != '\0')
     {
-        while (ft_isdigit(input[i]))
-            stock[next] = stock[next] * 10 + (input[i++] - '0');
-        if (ft_isdigit(input[i - 1]) && input[i] != ':')
+		ft_putchar_fd(input[i], 2);
+		if (ft_isdigit(input[i]))
+		{
+			while (ft_isdigit(input[i]))
+			{
+				ft_putchar_fd(input[i], 2);
+				ft_putchar_fd('\n', 2);
+				ft_putstr_fd("$$my_next = ", 2);
+				ft_putnbr_fd(next, 2);
+				ft_putchar_fd('\n', 2);
+				ft_putstr_fd("##stock[next0] = ", 2);
+				ft_putnbr_fd(stock[next], 2);
+				stock[next] = stock[next] * 10 + (input[i++] - '0');
+				ft_putchar_fd('\n', 2);
+				ft_putstr_fd(">>stock[next1] = ", 2);
+				ft_putnbr_fd(stock[next], 2);
+				ft_putchar_fd('\n', 2);
+			}
             next++;
+		}
         i++;
     }
+	next = 0;
 }
 
 static void ft_get_number_player(t_data *data, char *line)
@@ -46,11 +76,21 @@ static void ft_get_number_player(t_data *data, char *line)
     int i;
 
     i = 0;
+    data->num_player = 0;
     if (line[0] == '$')
+    {
         if (ft_strstr(line, "vomnes.filler"))
+        {
             while (line[i++])
+            {
                 if (line[i] == 'p' && ft_isdigit(line[i + 1]))
+                {
                     data->num_player = line[i + 1] - '0';
+                    data->player_shape = data->num_player == 2 ? 'X' : 'O';
+                }
+            }
+        }
+    }
 }
 
 static int ft_get_plateau(t_data *data, char *line)
@@ -70,6 +110,7 @@ static int ft_get_plateau(t_data *data, char *line)
         get_next_line(0, &str);
         while (i < data->xy_plateau[0])
         {
+			ft_putendl_fd("Stock plateau", 2);
             get_next_line(0, &str);
             if (ft_strchr(str, data->player_shape) && data->ok_min_y == 0)
             {
@@ -81,6 +122,7 @@ static int ft_get_plateau(t_data *data, char *line)
         }
         free(str);
         data->plateau[i] = NULL;
+		ft_print_strtab(data->plateau);
     }
     return (0);
 }
@@ -108,12 +150,17 @@ static int ft_get_piece(t_data *data, char *line)
     if ((ft_strstr(line, "Piece")))
     {
     //    ft_print_strtab(data->plateau);
+		ft_putendl_fd("In >> piece", 2);
+		ft_putendl_fd(line, 2);
         ft_get_xy(line, data->xy_piece);
         if (!(data->piece = (char**)ft_memalloc(sizeof(*data->piece)
         * (data->xy_piece[0] + 1))))
             return (-1);
+		ft_putendl_fd("In >> piece >> malloc done :)", 2);
+		dprintf(2, "xy_piece > %2d\n", data->xy_piece[0]); 
         while (i < data->xy_piece[0])
         {
+			ft_putendl_fd("Stock piece", 2);
             get_next_line(0, &str);
             data->piece[i] = ft_strdup(str);
             ft_count_coord(str, data);
@@ -128,17 +175,28 @@ static int ft_get_piece(t_data *data, char *line)
         {
             ft_get_coord_piece(data);
             ft_get_best_position(data);
+			ft_putendl_fd("Print plateau:", 2);
+			ft_print_strtab(data->plateau);
+			ft_putendl_fd("Plateau printed", 2);
+            ft_print_strtab(data->piece);
+            ft_putnbr_fd(data->final_pos_y, 2);
+            ft_putchar_fd(' ', 2);
+            ft_putnbr_fd(data->final_pos_x, 2);
+            ft_putchar_fd('\n', 2);
         //    ft_print_strtab(data->piece);
             ft_putnbr(data->final_pos_y);
             ft_putchar(' ');
             ft_putnbr(data->final_pos_x);
             ft_putchar('\n');
+			ft_display_position(data, '$');
         //    ft_print_strtab(data->plateau);
         //    ft_printf("%d %d\n", data->final_pos_y, data->final_pos_x); --> Kill Filler Segfault
             //ft_printf(">>%d<<\n", data->nb_coord);
             data->nb_coord = 0;
+			//	data->xy_piece[0] = 0;
+			//	data->xy_piece[1] = 0;
         //    ft_print_strtab(data->plateau);
-        //    ft_print_strtab(data->piece);
+        ft_print_strtab(data->piece);
         }
     }
     return (0);
